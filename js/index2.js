5486777f7a3b956b91c38cc1d1618381487a833a
gabmart1995 <gabmart1995@gmail.com>
2020-04-28 20:45:35 -0400

    wip: colocacion de font, canvas y referencias

---
+++ js/index2.js
@@ -0,0 +1,27 @@
+function drawLine( color, xInit, xFinish, yInit, yFinish, pad  ) {
+	pad.beginPath();
+	pad.strokeStyle = color;
+	pad.moveTo( xInit, yInit );
+	pad.lineTo( xFinish, yFinish );
+	pad.stroke();
+	pad.closePath();
+}
+
+const eifel = document.getElementById('eifel');
+const context = eifel.getContext('2d');
+
+console.log( context );
+
+const lines = 30;
+let count = 0;
+
+let yI = 0;
+let xF = 0;
+
+for ( count = 0; count < lines; count++ ) {
+	yI = 10 * count;
+	xF = 10 * ( count + 1 );
+
+	drawLine( '#17202a', 0, yI, xF, 300, context );
+	drawLine( '#17202a', 300, yI, xF, 0, context );
+}

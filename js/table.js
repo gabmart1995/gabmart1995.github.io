8f823c399dfe5c24a4126e0ce647462d6f88dbf7
gabmart1995 <gabmart1995@gmail.com>
2020-05-08 22:16:13 -0400

    wip: ajustes de modal de tabla

--- js/table.js
+++ js/table.js
@@ -1,4 +1,19 @@
 // TODOS
+let todos = [];
+const form = document.forms['form-table'];
+const table = document.getElementById('tbody');
+  
+form.addEventListener( 'submit', createTodo );
+
+class Todo {  
+  constructor( name, description ) {
+    this.name = name || '';
+    this.description = description || '';
+    this.id = new Date().getTime();
+    this.completed = false;
+  }
+}
+
 function showTable() {
   
     table.innerHTML = '';
@@ -17,7 +32,14 @@ function showTable() {
                 `<i class="fas fa-times w3-margin-right" onclick="completeTodo( ${ element.id } )"></i>` : 
                 `<i class="fas fa-check w3-margin-right" onclick="completeTodo( ${ element.id } )"></i>` 
               }
+              <i class="fas fa-pen w3-margin-right" onclick="showModal( true, {
+                id: ${ element.id },
+                name: '${ element.name }',
+                description: '${ element.description }'
+                })"
+              ></i>
               <i class="fas fa-trash" onclick="removeTodo( ${ element.id } )"></i>
+        
             </td>
           </tr>
         `
@@ -26,65 +48,75 @@ function showTable() {
     } 
   }
   
-  function removeTodo( todoId ) {
-    
-    todos = todos.filter( ( todo ) => todo.id !== todoId );
-    showTable();
-  }
+function removeTodo( todoId ) {
   
-  function submit( $event ) {
-    
-    const formulario = new FormData( form );
-    
-    let todo = {
-      id: new Date().getTime(),
-      name: formulario.get('todoName'),
-      description: formulario.get('todoDescription'),
-      completed: false
-    };
+  todos = todos.filter( ( todo ) => todo.id !== todoId );
+  showTable();
+}
+
+function createTodo( $event ) {
+
+  const formulario = new FormData( form );
+
+  todos.push(  
+    new Todo( formulario.get('name'), formulario.get('description') ) 
+  );
+
+  form[0].value = '';
+  form[1].value = '';
+
+  showTable();
+
+  $event.preventDefault();
+}
+
+function completeTodo( todoId ) {
   
-    todos.push( todo );
-    showTable();
-
-    $event.preventDefault();
-  }
-
-  function completeTodo( todoId ) {
-    
-    todos = todos.map( ( todo ) => {
-        
-        if ( todo.id === todoId ) {
+  todos = todos.map( ( todo ) => {
       
-          const totalTodo = document.getElementsByTagName('span');
-          addCompleted( totalTodo[0], todo.completed );
-
-          return {
-            ...todo,
-            completed: !todo.completed
-          };
-
-        } else {
-            return todo;
-        }
-    });
-
-    showTable();
-  }
-
-  function addCompleted( span, completed ) {
-   
-    if ( completed ) {
-      span.innerText = parseInt( span.innerText ) + 1
+      if ( todo.id === todoId ) {
     
-    } else {
-      span.innerText = parseInt( span.innerText ) - 1
-    }
-  }
-    
-  let todos = [];
-  const form = document.getElementById('form');
-  const table = document.getElementById('tbody');
+        const totalTodo = document.getElementsByTagName('span');
+        addCompleted( totalTodo[0], !todo.completed );
+
+        return {
+          ...todo,
+          completed: !todo.completed
+        };
+
+      } else {
+          return todo;
+      }
+  });
+
+  showTable();
+}
+
+function editTodo( todo ) {
+  console.log( todo );
+}
+
+
+function addCompleted( span, completed ) {
   
-  form.addEventListener( 'submit', submit );
+  if ( completed ) {
+    span.innerText = parseInt( span.innerText ) + 1
+  
+  } else {
+    span.innerText = parseInt( span.innerText ) - 1
+  }
+}
+
+function showModal( action, todo = null ) {
+
+  const modal = document.getElementById('modal-table');
+  
+  if ( action && todo !== null ) {
+    modal.style.display = 'block';
+  
+  } else {
+    modal.style.display = 'none';
+  }
+}
   
   

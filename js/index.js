cc525d74b9b38fcf91e4cec020f5353468ee916b
gabmart1995 <gabmart1995@gmail.com>
2020-06-19 19:17:00 -0400

    wip: se coloca imagen responsive

--- js/index.js
+++ js/index.js
@@ -4,18 +4,18 @@ function showList() {
     
   const nodes = document.getElementsByClassName('list');
 
-    for ( node of nodes ) {
-      
-      if ( node.style.display === 'none' ) {
-        node.style.display = 'block';
-      }
-
-      else if (  node.style.display === 'display' ) {
-        node.style.display = 'none';
-      }
-
-      else {
-        node.style.display = 'none';
-      }
+  for ( node of nodes ) {
+    
+    if ( node.style.display === 'none' ) {
+      node.style.display = 'block';
     }
+
+    else if (  node.style.display === 'display' ) {
+      node.style.display = 'none';
+    }
+
+    else {
+      node.style.display = 'none';
+    }
+  }
 }

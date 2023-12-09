40c74f6eed7f1ece12b023fa9e65747a393eeaf5
Gabriel Martinez <gabmart1995@gmail.com>
2023-12-09 15:41:25 -0400

    wip: ajustes

--- js/translate.js
+++ js/translate.js
@@ -45,9 +45,6 @@ const formatDocument = document.querySelector('html').cloneNode(true);
 
                 // inicializamos todos los eventos en cada cambio
                 changeTranslate();
-                initSidebar();
-                initMap();
-                initYear();
             })
             .catch(console.error);
     };
@@ -62,59 +59,6 @@ const formatDocument = document.querySelector('html').cloneNode(true);
         });
 
     }
-
-    /** Inicializa el menu lateral */
-    const initSidebar = () => {
-        const menu = document.querySelector('#menu');
-        if (!menu) return;
-
-        const sidenav = document.querySelector('#sidenav');
-        if (!sidenav) return;
-        
-        const closeButtonSidenav = sidenav.querySelector('.closebtn');
-        if (!closeButtonSidenav) return;
-        
-        // boton de apertura sidenav
-        menu.addEventListener('click', () => {
-            sidenav.style.width = '250px';
-        });
-
-        // boton de cierre sidenav
-        closeButtonSidenav.addEventListener('click', () => {
-            sidenav.style.width = '0';
-        });
-    };
-
-    /** inicializa el footer */
-    const initYear = () => {
-        const year = document.querySelector('#year');
-        
-        if (year) {
-        year.innerText = (new Date()).getFullYear(); 
-        }
-    };
-
-    /** inicializa el mapa */
-    const initMap = () => {
-        const mapElement = document.querySelector('#map');
-        if (!mapElement) return;
-
-        const map = L.map(mapElement);
-        let coords = [10.506872202554534, -66.91478277569756];
-        let zoom = 15;
-
-        // añade el fondo al mapa
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        }).addTo(map);
-
-        L.marker(coords).addTo(map)
-        .bindPopup('<span style="color: black;">Caracas, Venezuela</span>')
-        .openPopup();
-
-        // establece la camara
-        map.setView(coords, zoom);
-    };
 
     initTranslate(language);
 })();
